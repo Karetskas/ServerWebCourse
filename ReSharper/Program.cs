2839ff48d06f62aec6c38e82e66b74891c21dc82
Karetskas_Evgeny <kub99999@yandex.ru>
2023-05-25 16:25:28 +0500

    Created "ReSharper" project.

---
+++ ReSharper/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Academits.Karetskas.ReSharper
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Merge sort.");
+            Console.WriteLine();
+            
+            int[] array = { 13, 3, 8, 1, 15, 2, 3, 7, 4 };
+
+            Console.WriteLine($"Array before sort: {string.Join(" ", array)}");
+            Console.WriteLine();
+
+            for (var i = 1; i <= array.Length; i *= 2)
+            {
+                var firstPointer = 0;
+                var secondPointer = i;
+                var resultArray = new int[array.Length];
+                var resultArrayPointer = 0;
+
+                while (firstPointer < array.Length)
+                {
+                    var firstPointerLimit = secondPointer < array.Length
+                        ? secondPointer
+                        : array.Length;
+
+                    var secondPointerLimit = firstPointerLimit + i < array.Length
+                        ? firstPointerLimit + i
+                        : array.Length;
+
+                    while(firstPointer < firstPointerLimit || secondPointer < secondPointerLimit)
+                    {
+                        var isFirstPointerLimit = (firstPointer >= array.Length && secondPointer < array.Length) || firstPointer == firstPointerLimit;
+                        var isSecondPointerLimit = (secondPointer >= array.Length && firstPointer < array.Length) || secondPointer == secondPointerLimit;
+                        var isFirstItemGreater = firstPointer < array.Length && secondPointer < array.Length && array[firstPointer] > array[secondPointer];
+
+                        if (isFirstPointerLimit || (!isSecondPointerLimit && isFirstItemGreater))
+                        {
+                            resultArray[resultArrayPointer] = array[secondPointer];
+                            resultArrayPointer++;
+                            secondPointer++;
+
+                            continue;
+                        }
+
+                        resultArray[resultArrayPointer] = array[firstPointer];
+                        resultArrayPointer++;
+                        firstPointer++;
+                    }
+                    
+                    firstPointer = secondPointer;
+                    secondPointer = firstPointer + i;
+                }
+
+                array = resultArray;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Array after sort: {string.Join(" ", array)}");
+        }
+    }
+}
