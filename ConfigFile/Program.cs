854b64e125f8ea1117e585042acd115ebf9744d0
Karetskas_Evgeny <kub99999@yandex.ru>
2023-05-25 16:50:21 +0500

    Created "ConfigFile" project.

---
+++ ConfigFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+
+namespace Academits.Karetskas.ConfigFile
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var siteUrl = ConfigurationManager.AppSettings["SiteUrl"];
+
+            Console.WriteLine($"Site URL: {siteUrl}");
+            
+            Console.ReadKey();
+        }
+    }
+}
