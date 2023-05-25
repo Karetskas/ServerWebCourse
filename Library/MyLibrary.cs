79fb4578b6ec42b676a3054912a466fc0754940c
Karetskas_Evgeny <kub99999@yandex.ru>
2023-05-25 22:06:29 +0500

    Created "MyProgram" and "Library" projects.

---
+++ Library/MyLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Academits.Karetskas.TaskToSignAssembly.Library
+{
+    public static class MyLibrary
+    {
+        public static void PrintToConsole(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ MyProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Academits.Karetskas.TaskToSignAssembly.Library;
+
+namespace Academits.Karetskas.TaskToSignAssembly.MyProgram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MyLibrary.PrintToConsole("Calling the \"PrintToConsole\" command from the library.");
+
+            Console.ReadKey();
+        }
+    }
+}
