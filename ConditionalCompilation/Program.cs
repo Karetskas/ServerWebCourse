efd58c4997a64d1acc5489abda6e4bfc10689ed1
Karetskas_Evgeny <kub99999@yandex.ru>
2023-05-26 15:50:14 +0500

    Created "ConditionalCompilation" project.

---
+++ ConditionalCompilation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Academits.Karetskas.ConditionalCompilation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region Conditional Compilation
+#if DEBUG
+            Console.WriteLine("Выполнение кода в Debug версии.");
+#else
+            Console.WriteLine("Выполнение кода в остальных версиях.");
+#endif
+            #endregion
+        }
+    }
+}
