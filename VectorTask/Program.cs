9c14b23a0d5ac70869083e3f14b278a327efabe8
Karetskas_Evgeny <kub99999@yandex.ru>
2023-06-13 20:45:09 +0500

    Added "VectorTask" project.

---
+++ VectorTask/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace Academits.Karetskas.VectorTask
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double[] components1 = { -12, 22, -32 };
+            double[] components2 = { -42, -24, -42, -44, -54 };
+            double[] components3 = { 1, 2, 3, 4 };
+
+            Vector mainVector = new Vector(components3);
+
+            Vector[] vectors =
+            {
+                new Vector(5),
+                new Vector(components1),
+                new Vector(5, components2),
+                new Vector(mainVector)
+            };
+
+            string[] stringsArray = new string[vectors.Length];
+            Vector additionResult = new Vector(mainVector);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                additionResult.Add(vectors[i]);
+
+                stringsArray[i] = $"{mainVector} + {vectors[i],27} = {additionResult}";
+
+                additionResult = new Vector(mainVector);
+            }
+
+            string title = "Additing of one vector from another:";
+            PrintToConsole(ConsoleColor.Red, title, stringsArray);
+
+            Vector subtractionResult = new Vector(mainVector);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                subtractionResult.Subtract(vectors[i]);
+
+                stringsArray[i] = $"{mainVector} - {vectors[i],27} = {subtractionResult}";
+
+                subtractionResult = new Vector(mainVector);
+            }
+
+            title = "Subtraction of one vector from another:";
+            PrintToConsole(ConsoleColor.Yellow, title, stringsArray);
+
+            const double number = 2.1;
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                Vector multiplyingVectorByScalarResult = new Vector(vectors[i]);
+
+                multiplyingVectorByScalarResult.MultiplyByScalar(number);
+
+                stringsArray[i] = $"{vectors[i],27} * {number} = {multiplyingVectorByScalarResult}";
+            }
+
+            title = "Multiply a vector by a scalar:";
+            PrintToConsole(ConsoleColor.Green, title, stringsArray);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                Vector reversalResult = new Vector(vectors[i]);
+
+                reversalResult.Reverse();
+
+                stringsArray[i] = $"{vectors[i],27} * -1 = {reversalResult}";
+            }
+
+            title = "Vector reversal:";
+            PrintToConsole(ConsoleColor.Magenta, title, stringsArray);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                stringsArray[i] = $"{vectors[i],27} = {vectors[i].Length}";
+            }
+
+            title = "Get length of the vector:";
+            PrintToConsole(ConsoleColor.Blue, title, stringsArray);
+
+            stringsArray = new string[mainVector.Size];
+            Vector vectorToSetComponents = new Vector(mainVector);
+
+            for (int i = 0; i < vectorToSetComponents.Size; i++)
+            {
+                vectorToSetComponents[i] = 3;
+
+                stringsArray[i] = $"The index [{i}] in the {vectorToSetComponents} vector has been changed.";
+            }
+
+            title = "Set number \"3\" for the components of the vector:";
+            PrintToConsole(ConsoleColor.Cyan, title, stringsArray);
+
+            Vector vectorToGetComponents = new Vector(mainVector);
+
+            for (int i = 0; i < vectorToGetComponents.Size; i++)
+            {
+                stringsArray[i] = $"In the {vectorToGetComponents} vector: index [{i}] = {vectorToGetComponents[i]}";
+            }
+
+            title = "Get vector components by index:";
+            PrintToConsole(ConsoleColor.DarkBlue, title, stringsArray);
+
+            stringsArray = new string[vectors.Length];
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                stringsArray[i] = $"{mainVector} and {vectors[i],27} = {mainVector.Equals(vectors[i])}";
+            }
+
+            title = "Vectors comparison:";
+            PrintToConsole(ConsoleColor.DarkCyan, title, stringsArray);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                stringsArray[i] = $"Hash code of {vectors[i],27} vector = {vectors[i].GetHashCode()}";
+            }
+
+            title = "Get hash code of vectors:";
+            PrintToConsole(ConsoleColor.DarkGreen, title, stringsArray);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                stringsArray[i] = $"{mainVector} + {vectors[i],27} = {Vector.GetSum(mainVector, vectors[i])}";
+            }
+
+            title = "Addition of two vectors:";
+            PrintToConsole(ConsoleColor.DarkMagenta, title, stringsArray);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                stringsArray[i] = $"{mainVector} - {vectors[i],27} = {Vector.GetDifference(mainVector, vectors[i])}";
+            }
+
+            title = "Subtraction of two vectors:";
+            PrintToConsole(ConsoleColor.DarkRed, title, stringsArray);
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                stringsArray[i] = $"{mainVector} * {vectors[i],27} = {Vector.GetScalarProduct(mainVector, vectors[i])}";
+            }
+
+            title = "Scalar product of vectors:";
+            PrintToConsole(ConsoleColor.DarkYellow, title, stringsArray);
+        }
+
+        private static void PrintToConsole(ConsoleColor color, string title, string[] stringsArray)
+        {
+            Console.WriteLine(new string('-', 90));
+            Console.ForegroundColor = color;
+            Console.WriteLine(title);
+
+            foreach (string line in stringsArray)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,305 @@
+﻿using System;
+
+namespace Academits.Karetskas.VectorTask
+{
+    public sealed class Vector
+    {
+        private double[] components;
+
+        public int Size => components.Length;
+
+        public double Length
+        {
+            get
+            {
+                double componentsSquaredSum = 0;
+
+                foreach (double component in components)
+                {
+                    componentsSquaredSum += component * component;
+                }
+
+                return Math.Sqrt(componentsSquaredSum);
+            }
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= components.Length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"The \"{nameof(index)}\" = {index} argument is out of range of array. " +
+                        $"Valid range is from 0 to {components.Length - 1}.");
+                }
+
+                return components[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= components.Length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"The \"{nameof(index)}\" = {index} argument is out of range of array. " +
+                        $"Valid range is from 0 to {components.Length - 1}.");
+                }
+
+                components[index] = value;
+            }
+        }
+
+        public Vector(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException($"Argument \"{nameof(size)}\" <= 0.", nameof(size));
+            }
+
+            components = new double[size];
+        }
+
+        public Vector(double[] components)
+        {
+            if (components is null)
+            {
+                throw new ArgumentNullException($"{nameof(components)}", $"Argument of \"{nameof(components)}\" is null.");
+            }
+
+            if (components.Length == 0)
+            {
+                throw new ArgumentException($"Argument \"{nameof(components)}.{nameof(components.Length)}\" is 0.");
+            }
+
+            this.components = new double[components.Length];
+
+            components.CopyTo(this.components, 0);
+        }
+
+        public Vector(int size, double[] components)
+        {
+            if (components is null)
+            {
+                throw new ArgumentNullException(nameof(components), $"Argument of \"{nameof(components)}\" is null.");
+            }
+
+            if (size <= 0)
+            {
+                throw new ArgumentException($"Argument \"{nameof(size)}\" <= 0.", nameof(size));
+            }
+
+            this.components = new double[size];
+
+            Array.Copy(components, this.components, size);
+        }
+
+        public Vector(Vector vector)
+        {
+            if (vector is null)
+            {
+                throw new ArgumentNullException(nameof(vector), $"Argument \"{nameof(vector)}\" is null.");
+            }
+
+            components = new double[vector.Size];
+
+            vector.components.CopyTo(components, 0);
+        }
+
+        public void Add(Vector vector)
+        {
+            if (vector is null)
+            {
+                throw new ArgumentNullException(nameof(vector), $"Argument \"{nameof(vector)}\" is null.");
+            }
+
+            if (components.Length < vector.Size)
+            {
+                Array.Resize(ref components, vector.Size);
+            }
+
+            for (int i = 0; i < vector.Size; i++)
+            {
+                components[i] += vector[i];
+            }
+        }
+
+        public void Subtract(Vector vector)
+        {
+            if (vector is null)
+            {
+                throw new ArgumentNullException(nameof(vector), $"Argument \"{nameof(vector)}\" is null.");
+            }
+
+            if (components.Length < vector.Size)
+            {
+                Array.Resize(ref components, vector.Size);
+            }
+
+            for (int i = 0; i < vector.Size; i++)
+            {
+                components[i] -= vector[i];
+            }
+        }
+
+        public void MultiplyByScalar(double number)
+        {
+            for (int i = 0; i < components.Length; i++)
+            {
+                components[i] *= number;
+            }
+        }
+
+        public void Reverse()
+        {
+            MultiplyByScalar(-1);
+        }
+
+        public static Vector GetSum(Vector vector1, Vector vector2)
+        {
+            return vector1 + vector2;
+        }
+
+        public static Vector GetDifference(Vector vector1, Vector vector2)
+        {
+            return vector1 - vector2;
+        }
+
+        public static double GetScalarProduct(Vector vector1, Vector vector2)
+        {
+            if (vector1 is null)
+            {
+                throw new ArgumentNullException(nameof(vector1), $"Argument \"{nameof(vector1)}\" is null.");
+            }
+
+            if (vector2 is null)
+            {
+                throw new ArgumentNullException(nameof(vector2), $"Argument \"{nameof(vector2)}\" is null.");
+            }
+
+            int minVectorSize = Math.Min(vector1.Size, vector2.Size);
+            double scalarProduct = 0;
+
+            for (int i = 0; i < minVectorSize; i++)
+            {
+                scalarProduct += vector1[i] * vector2[i];
+            }
+
+            return scalarProduct;
+        }
+
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            if (vector1 is null)
+            {
+                throw new ArgumentNullException(nameof(vector1), $"Argument \"{nameof(vector1)}\" is null.");
+            }
+
+            if (vector2 is null)
+            {
+                throw new ArgumentNullException(nameof(vector2), $"Argument \"{nameof(vector2)}\" is null.");
+            }
+
+            Vector vectorsSum = new Vector(vector1);
+
+            vectorsSum.Add(vector2);
+
+            return vectorsSum;
+        }
+
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            if (vector1 is null)
+            {
+                throw new ArgumentNullException(nameof(vector1), $"Argument \"{nameof(vector1)}\" is null.");
+            }
+
+            if (vector2 is null)
+            {
+                throw new ArgumentNullException(nameof(vector2), $"Argument \"{nameof(vector2)}\" is null.");
+            }
+
+            Vector vectorsDifference = new Vector(vector1);
+
+            vectorsDifference.Subtract(vector2);
+
+            return vectorsDifference;
+        }
+
+        public static Vector operator *(Vector vector, double number)
+        {
+            if (vector is null)
+            {
+                throw new ArgumentNullException(nameof(vector), $"Argument \"{nameof(vector)}\" is null.");
+            }
+
+            Vector newVector = new Vector(vector);
+
+            newVector.MultiplyByScalar(number);
+
+            return newVector;
+        }
+
+        public static Vector operator *(double number, Vector vector)
+        {
+            if (vector is null)
+            {
+                throw new ArgumentNullException(nameof(vector), $"Argument \"{nameof(vector)}\" is null.");
+            }
+
+            Vector newVector = new Vector(vector);
+
+            newVector.MultiplyByScalar(number);
+
+            return newVector;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{string.Join(", ", components)}}}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if (obj is null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Vector vector = (Vector)obj;
+
+            if (components.Length != vector.components.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < components.Length; i++)
+            {
+                if (components[i] != vector[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 53;
+            int hash = 1;
+
+            hash = prime * hash + components.Length;
+
+            foreach (double component in components)
+            {
+                hash = prime * hash + component.GetHashCode();
+            }
+
+            return hash;
+        }
+    }
+}
